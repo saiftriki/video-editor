146f509eeb8407b3fa7719c729649698dbc8d698
saiftriki <saiftriki17@gmail.com>
2026-09-20 01:26:30 +0100

    add cluster mode

--- src/controllers/video.js
+++ src/controllers/video.js
@@ -1,4 +1,5 @@
 const path = require("node:path");
+const cluster = require("node:cluster");
 const crypto = require("node:crypto");
 const fs = require("node:fs/promises");
 const { pipeline } = require("node:stream/promises");
@@ -7,7 +8,10 @@ const DB = require("../DB");
 const FF = require("../../lib/FF");
 const JobQueue = require("../../lib/JobQueue");
 
-const jobs = new JobQueue();
+let jobs;
+if (cluster.isPrimary) {
+  jobs = new JobQueue();
+}
 
 // Return the list of all the videos that a logged in user has uploaded
 const getVideos = (req, res, handleErr) => {
@@ -118,12 +122,19 @@ const resizeVideo = async (req, res, handleErr) => {
   video.resizes[`${width}x${height}`] = { processing: true };
   DB.save();
 
-  jobs.enqueue({
-    type: "resize",
-    videoId,
-    width,
-    height,
-  });
+  if (cluster.isPrimary) {
+    jobs.enqueue({
+      type: "resize",
+      videoId,
+      width,
+      height,
+    });
+  } else {
+    process.send({
+      messageType: "new-resize",
+      data: { videoId, width, height },
+    });
+  }
 
   res.status(200).json({
     status: "success",
